f4b5729a256a7b8825714c401515e750e2868e59
lichinchen <che20014@byui.edu>
2025-01-07 16:25:26 -0500

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+        // Prompt the user to input their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user to input their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // display the result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+        
+
     }
 }
