c9d36166f93bdf94be9681765ddb9a8480a55eca
lichinchen <che20014@byui.edu>
2025-01-08 13:34:33 -0500

    Finish part1 part2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
-using System;
+using System;     // Basic functionality
+using System.Net; // Network functions
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");     // Ask for magic number
+        int magicNumber = int.Parse(Console.ReadLine()); // Read magic number
+
+        int guessNumber;                                 // Declare guessNumber variable
+        do
+        {
+            Console.Write("What is your guess? ");       // Ask for guess number
+            guessNumber = int.Parse(Console.ReadLine()); // Read guess number
+    
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");             // Guess higher
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");              // Guess lower
+            }
+            else
+            {
+                Console.WriteLine("You guess it!");      // Correct guess
+            }
+        } while (magicNumber != guessNumber);            // Loop until guess correct
+        
     }
 }
