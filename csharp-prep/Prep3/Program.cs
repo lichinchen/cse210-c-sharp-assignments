1915a19769ac3821729dbe1438cec39225d329d7
lichinchen <che20014@byui.edu>
2025-01-08 13:54:21 -0500

    Finish part 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,13 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");     // Ask for magic number
-        int magicNumber = int.Parse(Console.ReadLine()); // Read magic number
+        //Console.Write("What is the magic number? ");     // Ask for magic number
+        //int magicNumber = int.Parse(Console.ReadLine()); // Read magic number
+        Random randomGenerator = new Random();             // Create random number
+        int magicNumber = randomGenerator.Next(1, 101);    // Generate random number (1 to 100)
+        //Console.WriteLine(magicNumber);                    // Display the random number(test)
 
         int guessNumber;                                 // Declare guessNumber variable
         do
         {
-            Console.Write("What is your guess? ");       // Ask for guess number
+            Console.Write("What is your guess(1-100)? ");       // Ask for guess number
             guessNumber = int.Parse(Console.ReadLine()); // Read guess number
     
             if (magicNumber > guessNumber)
