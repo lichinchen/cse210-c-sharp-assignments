21961b50015c4779418944aad0541f494086c7ec
lichinchen <che20014@byui.edu>
2025-01-08 14:14:03 -0500

    Added number of guesses

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,27 +9,33 @@ class Program
         //int magicNumber = int.Parse(Console.ReadLine()); // Read magic number
         Random randomGenerator = new Random();             // Create random number
         int magicNumber = randomGenerator.Next(1, 101);    // Generate random number (1 to 100)
-        //Console.WriteLine(magicNumber);                    // Display the random number(test)
+        //Console.WriteLine(magicNumber);                  // Display the random number(test)
 
-        int guessNumber;                                 // Declare guessNumber variable
+        int guessNumber;                                   // Declare guessNumber variable
+        int trackGuess = 0;                                // Initiallize the number of guesses
         do
         {
-            Console.Write("What is your guess(1-100)? ");       // Ask for guess number
-            guessNumber = int.Parse(Console.ReadLine()); // Read guess number
+            Console.Write("What is your guess(1-100)? ");  // Ask for guess number
+            guessNumber = int.Parse(Console.ReadLine());   // Read guess number
     
             if (magicNumber > guessNumber)
             {
-                Console.WriteLine("Higher");             // Guess higher
+                Console.WriteLine("Higher");               // Guess higher
+                trackGuess += 1;
             }
             else if (magicNumber < guessNumber)
             {
-                Console.WriteLine("Lower");              // Guess lower
+                Console.WriteLine("Lower");                // Guess lower
+                trackGuess += 1;
             }
             else
             {
-                Console.WriteLine("You guess it!");      // Correct guess
+                Console.WriteLine("You guess it!");        // Correct guess
+                trackGuess += 1;
             }
-        } while (magicNumber != guessNumber);            // Loop until guess correct
+        } while (magicNumber != guessNumber);              // Loop until guess correct
+
+        Console.WriteLine($"You guessed {trackGuess} times!");
         
     }
 }
