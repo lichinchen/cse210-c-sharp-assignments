a6fce3c06d98b4d9c1720ebc070def5b0d134ca9
lichinchen <che20014@byui.edu>
2025-03-30 15:45:49 -0400

    Added code comments

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -1,15 +1,26 @@
-using System;
+using System;  // Import system namespace
 
+// Tasks of the Event Class
+// Store event details
+// Constructor – It sets up the event with the given details.
+// Show standard event details 
+// Show full event details 
+// Show a short event details
+
+// Defines an Event class
 public class Event
 {
-    private string _title;
+    // Stores event title, description, date, time, address
+    private string _title;  
     private string _description;
     private string _date;
     private string _time;
     private Address _address;
 
+    // Constructor
     public Event(string title, string description, string date, string time, Address address)
     {
+        // Assigns title, description, date, time, address value
         _title = title;
         _description = description;
         _date = date;
@@ -17,16 +28,19 @@ public class Event
         _address = address;
     }
 
+    // Returns standard event details
     public string GetStandardDetails()
     {
         return $"Title: {_title}\nDescription: {_description}\nDate: {_date} { _time}\nAddress: {_address.GetAddress()}";
     }
 
+    // Returns full event details
     public virtual string GetFullDetails()
     {
         return $"{GetStandardDetails()}\nEvent Type: {GetType().Name}";
     }
 
+    // Returns a short event details
     public string GetShortDescription()
     {
         return $"Event Type: {GetType().Name}\nName: {_title}\nDate: {_date}";
