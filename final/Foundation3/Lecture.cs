0289d75e1c7f070ba01bed841664c2d6ff5028ca
lichinchen <che20014@byui.edu>
2025-03-30 16:17:22 -0400

    Added code comments

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,20 +1,31 @@
+using System;  // Import system namespace
+
+// Tasks of this class
+// Inherits from the Event class.
+// Defines private fields--speaker, capacity
+// Constructor
+// Calls GetFullDetails() from the base class.
+// Display speaker and capacity.
+
+// Lecture class inherits Event
 public class Lecture : Event
 {
-    private string _speaker;
-    private int _capacity;
+    private string _speaker;  // Speaker's name
+    private int _capacity;    // Event's capacity
 
+    // Constructor
     public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
-        : base(title, description, date, time, address)
+        : base(title, description, date, time, address)  // Call base class constructor
     {
-        _speaker = speaker;
-        _capacity = capacity;
+        _speaker = speaker;    // Set the speaker's name
+        _capacity = capacity;  // Set the event's capacity
     }
 
+    // Method to get full lecture details
     public void GetLectureFullDetails()
     {
-        Console.WriteLine(GetFullDetails());
-        //GetFullDetails();
-        Console.WriteLine($"Speaker: {_speaker}");
-        Console.WriteLine($"Capacity: {_capacity}");
+        Console.WriteLine(GetFullDetails());          // Call the base class method to get full event details
+        Console.WriteLine($"Speaker: {_speaker}");    // Display speaker
+        Console.WriteLine($"Capacity: {_capacity}");  // Display capacity
     }
 }
