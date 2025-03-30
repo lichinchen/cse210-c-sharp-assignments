666b6c6dcc2101a85d99c2b8287796c71fe24103
lichinchen <che20014@byui.edu>
2025-03-30 16:46:29 -0400

    Added code comments

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -1,17 +1,27 @@
+using System;  // Import system namespace
+
+// Inherits from the Event class.
+// Stores an RSVP email address. 
+// Constructor
+// Display full event details, including the RSVP email
+
+
+// Inherits from Event class
 public class Reception : Event
 {
-    private string _rsvpEmail;
+    private string _email;  // Stores the email
 
-    public Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
-        : base(title, description, date, time, address)
+    // Constructor
+    public Reception(string title, string description, string date, string time, Address address, string Email)
+        : base(title, description, date, time, address)   // Calls base class constructor
     {
-        _rsvpEmail = rsvpEmail;
+        _email = Email;  // Sets the email
     }
 
+    // Method to display full reception details
     public void GetReceptionsFullDetails()
     {
-        Console.WriteLine(GetFullDetails());
-        //GetFullDetails();
-        Console.WriteLine($"RSVP Email: {_rsvpEmail}");
+        Console.WriteLine(GetFullDetails());         // Call the base class method to get full event details
+        Console.WriteLine($"RSVP Email: {_email}");  // Displays RSVP email
     }
 }
