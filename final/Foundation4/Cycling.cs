5e98119ee4d2198b040964bd25e94d94e3f46bd1
lichinchen <che20014@byui.edu>
2025-03-30 20:53:27 -0400

    Added code comments

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -1,25 +1,39 @@
+using System;  // Import system namespace
+
+// Inherits from Activity
+// Stores Speed
+// Constructor
+// Calculates Distance
+// Returns Speed
+// Calculates Pace
+
+// Cycling class inherits from Activity
 public class Cycling : Activity
 {
-    private double _speed;
+    private double _speed;  // Stores the speed
 
+    // Constructor
     public Cycling(string date, int minutes, double speed) 
-        : base(date, minutes)
+        : base(date, minutes)  // Call the base class constructor
     {
-        _speed = speed;
+        _speed = speed;  // Set the speed
     }
 
+    // Override Distance method
     public override double Distance()
     {
-        return _speed * GetMinutes() / 60; // Distance in miles
+        return _speed * GetMinutes() / 60;   // Distance in miles (speed * time in hours)
     }
 
+    // Override Speed method
     public override double Speed()
     {
-        return _speed; // Speed is directly provided
+        return _speed;  // Return the speed value
     }
-
+    
+    // Override Pace method
     public override double Pace()
     {
-        return 60 / _speed; // Pace in minutes per mile
+        return 60 / _speed;   // Pace in minutes per mile
     }
 }
