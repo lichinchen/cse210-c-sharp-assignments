e4f7cbbcf27041b190287d1c3fafd11c1acbf4f0
lichinchen <che20014@byui.edu>
2025-03-30 20:37:01 -0400

    Added code comments

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,33 +1,38 @@
-using System;
+using System;  // Import system namespace
 
 public abstract class Activity
 {
-    private string _date;
-    private int _minutes;
+    private string _date;  // Stores the date
+    private int _minutes;  // Stores the minutes
 
+    // Constructor
     public Activity(string date, int minutes)
     {
-        _date = date;
-        _minutes = minutes;
+        _date = date;        // Set the date
+        _minutes = minutes;  // Set the minutes
     }
 
+    // Method to get the minutes
     public int GetMinutes()
     {
         return _minutes;
     }
 
+     // Abstract for calculating distance
     public abstract double Distance();
 
+    // Abstract for calculating speed
     public abstract double Speed();
 
+    // Abstract for calculating pace
     public abstract double Pace();
 
+    // Get a summary of the activity
     public string GetSummary()
-    {
-        //return $"{_date} {GetType().Name} ({_minutes} min): Distance {Distance()}, Speed {Speed()}, Pace {Pace()}";
-        return $"{_date} {GetType().Name} ({_minutes} min)\n" +
-               $"- Distance: {Distance():F2} miles\n" +
-               $"- Speed: {Speed():F2} mph\n" +
-               $"- Pace: {Pace():F2} min/mile\n";
-    }
+    {                                                             // GetType().Name--Get the object's type name
+        return $"{_date} {GetType().Name} ({_minutes} min)\n" +   // Return the date, activity type, and minutes 
+               $"- Distance: {Distance():F2} miles\n" +           // Format distance with 2 decimal 
+               $"- Speed: {Speed():F2} mph\n" +                   // Format speed with 2 decimal 
+               $"- Pace: {Pace():F2} min/mile\n";                 // Format pace with 2 decimal 
+    } 
 }
